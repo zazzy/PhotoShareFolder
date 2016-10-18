ef56e216f6a9a1579a3184b4c55b48c572a547b0
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-10-18 22:35:46 +0100

    index

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+    <html>
+    <head>
+    </head>
+    <body>
+    <p>
+        <?php
+        $myAge = 20;
+        echo "<a href=''>link</a>";
+        if($myAge > 16){
+            echo "You can have specs m8 <br />";
+        }else{
+            echo"Too young boyo<br />";
+        }
+        if($myAge >18){
+            echo "You can have mugs pal<br />";
+        }else{
+            echo"Too young boyo<br />";
+        }
+        if($myAge > 21) {
+            echo "some shit<br />";
+        }else{
+            echo "Too young boyo<br />";
+        }
+        ?>
+    </p>
+    </body>
+</html>
