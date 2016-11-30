7d0dd1fb7ddee6a4a8c115de6d1dec22d6be4eff
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-11-30 14:32:15 +0000

    css test

--- profile.php
+++ profile.php
@@ -1,3 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head><title>PhotoShare - Profile</title></head>
+<body>
+<div id ="profile_details">
+
+
+</div>
+<section id ="photo_view">
+    <img src="./img/img1.jpg"/>
+    <img src="./img/img2.jpg"/>
+    <img src="./img/img1.jpg"/>
+    <img src="./img/img3.jpg"/>
+    <img src="./img/img2.jpg"/>
+    <img src="./img/img3.jpg"/>
+    <img src="./img/img3.jpg"/>
+    <img src="./img/img2.jpg"/>
+    <img src="./img/img1.jpg"/>
+    <img src="./img/img2.jpg"/>
+    <img src="./img/img2.jpg"/>
+    <img src="./img/img3.jpg"/>
+
+</section>
+</body>
+</html>
+
 <?php
 
-?>PROFILE
+?>
