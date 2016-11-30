c47f975dfb5d35a2654e8f3add066adc764acab8
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-11-30 12:41:06 +0000

    test?

--- global/nav.php
+++ global/nav.php
@@ -1,12 +1,12 @@
 
 <div id="nav">
 <ul>
-<li><a href="?page=home">Home</a></li>
-<li><a href="?page=profile">Profile</a></li>
-<li><a href="?page=gallery">Gallery</a></li>
-<li><a href="?page=login">Login</a></li>
-<li><a href="?page=register">Register</a></li>
-<li><a href="?page=about">About</a></li>
+<li class ="active"><a href="?page=home">Home</a></li>
+<li class ="active"><a href="?page=profile">Profile</a></li>
+<li class ="active"><a href="?page=gallery">Gallery</a></li>
+<li class ="active"><a href="?page=login">Login</a></li>
+<li class ="active"><a href="?page=register">Register</a></li>
+<li class ="active"><a href="?page=about">About</a></li>
 </ul>
 </div>
 <div id = "content">
