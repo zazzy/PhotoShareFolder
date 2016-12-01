12230ecc0568a9c0fe2d131b7b705c5d66838c3a
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-01 18:27:04 +0000

    upload revisited

--- login.php
+++ login.php
@@ -5,6 +5,7 @@ session_start();
 
 if(isset($_POST['login'])) {
     include("./db/connect.php");
+    $salt = "1238ImTheBoy23o"
     $username = strip_tags($_POST['username']);
     $password = strip_tags($_POST['password']);
 
@@ -14,7 +15,7 @@ if(isset($_POST['login'])) {
     $username = mysqli_real_escape_string($db,$username);
     $password = mysqli_real_escape_string($db,$password);
 
-    $password = md5($password);
+    $password = md5($password.$salt);
 
     $sql = "select * from user WHERE name='$username' LIMIT 1";
     $query = mysqli_query($db,$sql);
