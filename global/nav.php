99a04f2f5b76356a10f7768ab172ce40c2d50b9f
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-02 17:22:06 +0000

    rights

--- global/nav.php
+++ global/nav.php
@@ -11,8 +11,7 @@
         <li><a href="?page=about">About</a></li>
         <li><a href="?page=logout">Logout</a></li>
         <?php
-    }
-    if(!isset($_SESSION['username'])) {
+    }elseif(!isset($_SESSION['username'])) {
         ?>
         <li><a href="?page=home">Home</a></li>
         <li><a href="?page=gallery">Gallery</a></li>
@@ -22,8 +21,7 @@
         <li><a href="?page=about">About</a></li>
 
         <?php
-    }
-    if(isset($_SESSION['id']) && $_SESSION['id']=="1"){
+    }elseif(isset($_SESSION['id']) && $_SESSION['id']=="1"){
     ?>
         <li><a href="?page=home">Home</a></li>
         <li><a href="?page=profile">Profile</a></li>
@@ -31,7 +29,7 @@
         <li><a href="?page=admin">ADMIN</a></li>
         <li><a href="?page=about">About</a></li>
         <li><a href="?page=logout">Logout</a></li>
-    <?php } ?>
+    <?php}else{echo "NAV ERROR";}  ?>
 </ul>
 </div>
 <div id ="full_spacer">
