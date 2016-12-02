bcd604068e1171c28972df67f30e692b2dffdf44
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-02 17:37:52 +0000

    rights

--- global/nav.php
+++ global/nav.php
@@ -1,50 +1,37 @@
 
-
-<?php
-if(isset($_SESSION['username'])) {
-    echo $_SESSION['username'];
-?>
-<div id="nav" xmlns="http://www.w3.org/1999/html">
+<div id="nav">
 <ul>
+    <li><a href="?page=home">Home</a></li>
+    <li><a href="?page=gallery">Gallery</a></li>
+    <?php
+    if(!isset($_SESSION['username'])){
+        echo '
+    <li><a href="?page=login">Login</a></li>
+    <li><a href="?page=register">Register</a></li>
+        ';
+    }
 
-        <li><a href="?page=home">Home</a></li>
-        <li><a href="?page=profile">Profile</a></li>
-        <li><a href="?page=gallery">Gallery</a></li>
 
-        <li><a href="?page=about">About</a></li>
-        <li><a href="?page=logout">Logout</a></li>
+    if(isset($_SESSION['username'])){
+    echo '
+    <li><a href="?page=profile">Profile</a></li>
+    <li><a href="?page=logout">Logout</a></li>
+    ';
+    }
+
+
+    if($_SESSION['id']=="1"){
+        echo '
+    <li><a href="?page=admin">ADMIN</a></li>
+    ';
+    }
+    ?>
+
+    <li><a href="?page=about">About</a></li>
+
 
 </ul>
 </div>
-
-    <?php
-}elseif(!isset($_SESSION['username'])) {
-    ?>
-    <div id ='nav'>
-        <ul>
-
-    <li><a href="?page=home">Home</a></li>
-    <li><a href="?page=gallery">Gallery</a></li>
-
-    <li><a href="?page=login">Login</a></li>
-    <li><a href="?page=register">Register</a></li>
-    <li><a href="?page=about">About</a></li>
-        </ul>
-</div>
-    <?php
-}elseif($_SESSION['id']=="1"){
-    ?>
-    <div id ='nav'>
-        <ul>
-    <li><a href="?page=home">Home</a></li>
-    <li><a href="?page=profile">Profile</a></li>
-    <li><a href="?page=gallery">Gallery</a></li>
-    <li><a href="?page=admin">ADMIN</a></li>
-    <li><a href="?page=about">About</a></li>
-    <li><a href="?page=logout">Logout</a></li>
-            </ul>
-        </div>
-<?php}else{echo "NAV ERROR";}  ?>
 <div id ="full_spacer">
 
 </div>
