1d97737042aa41132ccb1c66c4bb63296f4ba953
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-02 17:10:20 +0000

    rights

--- global/nav.php
+++ global/nav.php
@@ -2,15 +2,25 @@
 <div id="nav">
 <ul>
     <?php
-    if()
+    if(isset($_SESSION['username'])) {
+        ?>
+        <li><a href="?page=home">Home</a></li>
+        <li><a href="?page=profile">Profile</a></li>
+        <li><a href="?page=gallery">Gallery</a></li>
+        <li><a href="?page=logout">Logout</a></li>
+        <li><a href="?page=about">About</a></li>
+        <?php
+    }
+    if(!isset($_SESSION['username'])) {
+        ?>
+        <li><a href="?page=register">Register</a></li>
+        <li><a href="?page=login">Login</a></li>
+        <?php
+    }
+    if(isset($_SESSION['id']) && $_SESSION['id']=="1"){
     ?>
-    <li><a href="?page=home">Home</a></li>
-    <li><a href="?page=profile">Profile</a></li>
-    <li><a href="?page=gallery">Gallery</a></li>
-    <li><a href="?page=login">Login</a></li>
-    <li><a href="?page=logout">Logout</a></li>
-    <li><a href="?page=register">Register</a></li>
-    <li><a href="?page=about">About</a></li>
+    <li><a href="?page=admin">ADMIN</a></li>
+    <?php } ?>
 </ul>
 </div>
 <div id ="full_spacer">
