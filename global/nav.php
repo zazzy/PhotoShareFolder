a8181471dd9adc8ebf02b592192e35d630a763b9
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-02 17:29:11 +0000

    rights

--- global/nav.php
+++ global/nav.php
@@ -1,41 +1,50 @@
 
-<div id="nav">
+
+<?php
+if(isset($_SESSION['username'])) {
+    echo $_SESSION['username'];
+?>
+<div id="nav" xmlns="http://www.w3.org/1999/html">
 <ul>
-    <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors',1);
 
-
-    if(isset($_SESSION['username'])) {
-        ?>
         <li><a href="?page=home">Home</a></li>
         <li><a href="?page=profile">Profile</a></li>
         <li><a href="?page=gallery">Gallery</a></li>
 
         <li><a href="?page=about">About</a></li>
         <li><a href="?page=logout">Logout</a></li>
-        <?php
-    }elseif(!isset($_SESSION['username'])) {
-        ?>
-        <li><a href="?page=home">Home</a></li>
-        <li><a href="?page=gallery">Gallery</a></li>
 
-        <li><a href="?page=login">Login</a></li>
-        <li><a href="?page=register">Register</a></li>
-        <li><a href="?page=about">About</a></li>
-
-        <?php
-    }elseif($_SESSION['id']=="1"){
-    ?>
-        <li><a href="?page=home">Home</a></li>
-        <li><a href="?page=profile">Profile</a></li>
-        <li><a href="?page=gallery">Gallery</a></li>
-        <li><a href="?page=admin">ADMIN</a></li>
-        <li><a href="?page=about">About</a></li>
-        <li><a href="?page=logout">Logout</a></li>
-    <?php}else{echo "NAV ERROR";}  ?>
 </ul>
 </div>
+
+    <?php
+}elseif(!isset($_SESSION['username'])) {
+    ?>
+    <div id ='nav'>
+        <ul>
+
+    <li><a href="?page=home">Home</a></li>
+    <li><a href="?page=gallery">Gallery</a></li>
+
+    <li><a href="?page=login">Login</a></li>
+    <li><a href="?page=register">Register</a></li>
+    <li><a href="?page=about">About</a></li>
+        </ul>
+</div>
+    <?php
+}elseif($_SESSION['id']=="1"){
+    ?>
+    <div id ='nav'>
+        <ul>
+    <li><a href="?page=home">Home</a></li>
+    <li><a href="?page=profile">Profile</a></li>
+    <li><a href="?page=gallery">Gallery</a></li>
+    <li><a href="?page=admin">ADMIN</a></li>
+    <li><a href="?page=about">About</a></li>
+    <li><a href="?page=logout">Logout</a></li>
+            </ul>
+        </div>
+<?php}else{echo "NAV ERROR";}  ?>
 <div id ="full_spacer">
 
 </div>
