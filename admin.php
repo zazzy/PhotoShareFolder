f0957d0ab956405d6531e58cda6c0693c18fd57e
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-03 18:22:21 +0000

    rights

--- admin.php
+++ admin.php
@@ -12,6 +12,26 @@ session_start();
 <?php
 
 echo "TESTTTTT";
+$query = mysql_query("select * from user");
+
+echo "<table>";
+while($rows = mysql_fetch_array($query)) {
+    echo"    <tr>
+<td > Username: </td >
+<td > ".$rows['name']." </td >
+</tr >
+
+<tr >
+<td > ID: </td >
+<td > ".$rows['id']." </td >
+</tr >
+
+<tr >
+<td > Role: </td >
+<td > ".$rows['role']." </td >
+</tr >";
+}
+echo "</table>";
 
 ?>
 
