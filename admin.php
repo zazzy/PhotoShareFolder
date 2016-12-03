d5247cac3090609f19598e9dcdb1b9f19ee35a71
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-03 18:18:45 +0000

    rights

--- admin.php
+++ admin.php
@@ -8,35 +8,20 @@ session_start();
     <title>Admin</title>
 </head>
 <body>
-<form action="" method="post" enctype="multipart/form-data">
-    <input placeholder="Username" name="username" type="text" autofocus>
-    <input placeholder="Password" name="password" type="password" autofocus>
-    <input name="login" type="submit" autofocus>
-</form>
 
 <?php
-/*
+
 $query = mysql_query("select * from user");
 
 echo "<table>";
 while($rows = mysql_fetch_array($query)) {
 echo"    <tr>
 <td > Username: </td >
-<td > ".$rows['name']." </td >
-</tr >
 
-<tr >
-<td > ID: </td >
-<td > ".$rows['id']." </td >
-</tr >
-
-<tr >
-<td > Role: </td >
-<td > ".$rows['role']." </td >
 </tr >";
 }
 echo "</table>";
-*/
+
 ?>
 
 </div>
