2024e910230ff05e3ec3ee7257562b6331f1e895
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-04 16:59:45 +0000

    on image click

--- profile.php
+++ profile.php
@@ -66,10 +66,10 @@ include("./db/uploads/uploadFile.php");
     echo "<img id ='userImg' src=./".$image.">";
         echo "<div id = fullholderDetails><table>";
         echo "<br>";
-        echo "<tr><td>Name: ".$imageName."<td><td></td><tr>";
-        echo "<tr><td>Description: ".$imageDesc."<td><td></td><tr>";
-        echo "<tr><td>User ID: " .$imageUID."</td><td></td><tr>";
-        echo "<tr><td>Price: ".$imagePrice."</td><td></td><tr>";
+        echo "<tr><td>Name: ".$imageName."</td><br><td></td><tr>";
+        echo "<tr><td>Description: ".$imageDesc."</td><br><td></td><tr>";
+        echo "<tr><td>User ID: " .$imageUID."</td><br><td></td><tr>";
+        echo "<tr><td>Price: ".$imagePrice."</td><br><td></td><tr>";
         echo "</table></div>";
         echo "</div>";
     }
