995637fbc0b6ac8f575f892b4b8157343a7c15a9
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-04 20:40:00 +0000

    on image click

--- gallery.php
+++ gallery.php
@@ -5,19 +5,48 @@
 <body>
 
 <section id ="photo_view">
-<?php
-include("./db/connect.php");
+    <?php
+    include("./db/connect.php");
 
-$PDO = new PDO($dsn,"b7716a5fb7c215","2471e43b096d840");
-$handler = $PDO->prepare("select * from image");
+    $PDO = new PDO($dsn,"b7716a5fb7c215","2471e43b096d840");
+    $handler = $PDO->prepare("select * from image where user_id =".$_SESSION['id']);
 
-$handler->execute();
+    $handler->execute();
+    $userHandler = $PDO->prepare("select * from user");
+    $userHandler->execute();
+    $imageU = "UN";
+    while($row = $handler->fetch(PDO::FETCH_ASSOC)){
+        $image=$row['imgurl'];
+        $imageName = $row['file_name'];
+        $imageDesc =$row['Description'];
 
-while($row = $handler->fetch(PDO::FETCH_ASSOC)) {
-    $image = $row['imgurl'];
-    echo "<img src=./" . $image . ">";
-}
-?>
+        while($row2 = $userHandler->fetch(PDO::FETCH_ASSOC)){
+            if($row2['id']==$row['user_id']){
+                $imageU =$row2['name'];
+            }}
+
+
+
+        $imagePrice = $row['price'];
+        echo "<div id = fullholder>";
+        echo "<img id ='userImg' src=./".$image.">";
+        echo "<div id = fullholderDetails><table>";
+
+        echo "<tr><td>Name: ".$imageName."</td><tr>";
+
+        echo "<tr><td>Desc: ".$imageDesc."</td><tr>";
+
+        echo "<tr><td>User: " .$imageU."</td><tr>";
+
+        echo "<tr><td>Price: £".$imagePrice."</td><tr>";
+        echo "</table></div>";
+        echo "</div>";
+    }
+
+    ?>
+
+
+
 </section>
 </div>
 </body>
