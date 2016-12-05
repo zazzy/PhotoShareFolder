93aefa2750335c4a3a7dda594ca3fd9f99821df4
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-05 12:42:01 +0000

    gallery fix

--- paypal.php
+++ paypal.php
@@ -3,47 +3,20 @@
 <body>
 
 
-<script src="https://www.paypalobjects.com/api/checkout.js" data-version-4></script>
 
-<div id = "paypal-button">
-    <script>
-        paypal.Button.render({
 
-            env: 'production', // Optional: specify 'sandbox' environment
 
-            client: {
-                sandbox:    'https://www.sandbox.paypal.com/checkoutnow?token=EC-HYU6F8FDBFFD3GLD',
-                production: 'https://www.sandbox.paypal.com/checkoutnow?token=EC-HYU6F8FDBFFD3GLD'
-            },
+<form id="myContainer" method="post" action="/checkout"></form>
 
-            payment: function() {
+<script>
+    window.paypalCheckoutReady = function () {
+        paypal.checkout.setup('<Your-Merchant-ID>', {
+            environment: 'sandbox',
+            container: 'myContainer'
+        });
+    };
+</script>
 
-                var env    = this.props.env;
-                var client = this.props.client;
-
-                return paypal.rest.payment.create(env, client, {
-                    transactions: [
-                        {
-                            amount: { total: '1.00', currency: 'USD' }
-                        }
-                    ]
-                });
-            },
-
-            commit: true, // Optional: show a 'Pay Now' button in the checkout flow
-
-            onAuthorize: function(data, actions) {
-
-                // Optional: display a confirmation page here
-
-                return actions.payment.execute().then(function() {
-                    header('Location: index.php');
-                });
-            }
-
-        }, '#paypal-button');
-    </script>
-    </div>
-
+<script src="http://www.paypalobjects.com/api/checkout.js" async></script>
 </body>
 </html>
