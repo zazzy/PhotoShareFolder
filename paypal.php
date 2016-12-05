ec4671a90a4533b33f18dfe8f3b2461bceb80b5b
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-05 12:52:28 +0000

    gallery fix

--- paypal.php
+++ paypal.php
@@ -6,17 +6,12 @@
 
 
 
-<form id="myContainer" method="post" action="/checkout"></form>
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+    <input type="hidden" name="cmd" value="_s-xclick">
+    <input type="hidden" name="hosted_button_id" value="VG9AMTPN9467L">
+    <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+    <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
 
-<script>
-    window.paypalCheckoutReady = function () {
-        paypal.checkout.setup('AFcWxV21C7fd0v3bYYYRCpSSRl31A3GSQcpQJJ1ID3ySRrjOE2-kryGj', {
-            environment: 'sandbox',
-            container: 'myContainer'
-        });
-    };
-</script>
-
-<script src="http://www.paypalobjects.com/api/checkout.js" async></script>
 </body>
 </html>
