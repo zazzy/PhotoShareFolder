a484512b48401b3fcc7b4ae8b2d028454929e0af
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-05 15:35:45 +0000

    gallery fix

--- profile.php
+++ profile.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
+/*
 error_reporting(E_ALL);
 ini_set('display_errors',1);
+*/
 /* fucked fix me lol */
 include("./db/connect.php");
 include("./db/uploads/uploadFile.php");
