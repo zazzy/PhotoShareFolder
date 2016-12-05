54f5540050c8275d41912ae85c17fb287f017e60
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-05 16:29:35 +0000

    gallery fix

--- profile.php
+++ profile.php
@@ -105,24 +105,23 @@ include("./db/uploads/uploadFile.php");
 
         echo "</div>";
         ?>
+        <form name="_xclick" action="https://www.paypal.com/uk/cgi-bin/webscr" method="post">
+            <input type="hidden" name="cmd" value="_xclick">
+            <input type="hidden" name="business" value="robertharrisjjus@hotmail.co.uk">
+            <input type="hidden" name="currency_code" value="GBP">
+            <input type="hidden" name="item_name" value="<?php echo $imageDesc; ?>">
+            <input type="hidden" name="amount" value="12.99">
+            <input type="image" src="http://www.paypal.com/en_GB/i/btn/x-click-but01.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+        </form>
         <!--
         <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
             <input type="hidden" name="cmd" value="_s-xclick">
             <input type="hidden" name="hosted_button_id" value="NUNTL6W54KY9A">
             <input type="hidden" name="amount" value="99.99">
-            <input type="hidden" name="item_name" value="<?php echo $imageDesc; ?>">
+            <input type="hidden" name="item_name" value="">
             <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
             <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-        </form>
-        -->
-        <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
-            <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="NUNTL6W54KY9A">
-            <input type="hidden" name="amount" value="99.99">
-            <input type="hidden" name="item_name" value="<?php echo $imageDesc; ?>">
-            <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-            <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-        </form>
+        </form>-->
 
         <?
 
