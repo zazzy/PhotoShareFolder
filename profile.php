4092ad43da613e37638b80d8773373f693658d47
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-05 15:46:56 +0000

    gallery fix

--- profile.php
+++ profile.php
@@ -107,8 +107,9 @@ include("./db/uploads/uploadFile.php");
         ?>
         <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
             <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="VG9AMTPN9467L">
+            <input type="hidden" name="business" value="robertharrisjjus@hotmail.co.uk">
             <input type="hidden" name="amount" value="99">
+            <input type="hidden" name="currency_code" value="GBP">
             <input type="hidden" name="item_name" value="<?php echo $imageDesc; ?>">
             <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
             <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
