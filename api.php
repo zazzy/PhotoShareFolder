711d5875922bf8e6acaa81c339c55eacaca7d4cc
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-18 15:25:01 +0000

    API#plzwork

--- api.php
+++ api.php
@@ -19,35 +19,4 @@ function getAllUsers(){
     $users = json_encode($users);
     echo $_GET['jsoncallback'] . '(' . $users . ')';
 }
-?>
-<html>
-<head>
-    <title>API</title>
-</head>
-<body>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script type ="text/javascript">
-    $(function(){
 
-        $.ajax({
-            type: 'GET',
-            url: 'http://photoshare1418132.azurewebsites.net/api.php?method=getAllUsers&jsoncallback=?',
-            success: function (data) {
-                for(person in $.parseJSON(data)){
-                    var user = data[person]
-                    console.log(user)
-                }
-              //  console.dir('success', data);
-            }
-            })
-        });
-        //$.getJSON("http://photoshare1418132.azurewebsites.net/api.php?method=getAllUsers&jsoncallback=?");
-
-
-
-</script>
-
-
-
-</body>
-</html>
