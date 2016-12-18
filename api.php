111af1e873502438eacb6f661bd8262e28149b43
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-18 15:56:35 +0000

    API#plzwork

--- api.php
+++ api.php
@@ -8,19 +8,37 @@ include("db/connect.php");
     }
 
 
-function getAllUsers(){
+function getAllUsers()
+{
     global $db;
-    $user_sql = mysqli_query($db,"select * from USER");
+    $user_sql = mysqli_query($db, "select * from USER");
     $users = array();
 
-    while($row = mysqli_fetch_assoc($user_sql)){
+    while ($row = mysqli_fetch_assoc($user_sql)) {
         $users[] = $row;
     }
     echo json_encode($users);
-    //
-    //
-    ///$users = json_encode($users);
-    //echo $_GET['jsoncallback'] . '(' . $users . ')';
 }
+?>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script type ="text/javascript">
+    $(function(){
+
+        $.ajax({
+            type: 'GET',
+            url: 'http://photoshare1418132.azurewebsites.net/api.php?method=getAllUsers&jsoncallback=?',
+            success: function (data) {
+                for(person in data){
+                    var user = data[person]
+                    console.log(user)
+                }
+                //  console.dir('success', data);
+            }
+        })
+    });
+    //$.getJSON("http://photoshare1418132.azurewebsites.net/api.php?method=getAllUsers&jsoncallback=?");
 
 
+
+</script>
