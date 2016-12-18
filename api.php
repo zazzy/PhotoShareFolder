8e966e0ee7402b9672b2c715223c58919f14c32b
zazzy <zazzy-loves-penguin@hotmail.co.uk>
2016-12-18 13:51:46 +0000

    API#plzwork

---
+++ api.php
@@ -0,0 +1,41 @@
+<?php
+include("db/connect.php");
+
+
+
+    if(function_exists($_GET['method'])){
+        $_GET['method']();
+    }
+
+
+function getAllUsers(){
+    global $db;
+    $user_sql = mysqli_query($db,"select * from USER");
+    $users = array();
+    while(£singleUser = mysqli_fetch_array($user_sql)){
+        $users[] = £singleUser;
+    }
+    $users = json_encode($users);
+    echo $_GET['jsoncallback'] . '(' . $users . ')';
+}
+?>
+<html>
+<head>
+    <title>API</title>
+</head>
+<body>
+
+<script>
+    $(function(){
+        $.getJSON("http://photoshare1418132.azurewebsites.net/api.php?method=getAllUsers&jsoncallback=?")
+        function(data){
+            console.log(data);
+        }
+        )
+    });
+</script>
+
+
+
+</body>
+</html>
